d772c168b14c420c71312f680fc15e458957a48d
wanglei62 <wanglei62@lenovo.com>
2020-05-06 09:53:04 +0800

    springboot validator

---
+++ com-thd-springboottest-standardcode/src/main/java/com/thd/springboottest/standardcode/controller/ValidatorController.java
@@ -0,0 +1,63 @@
+package com.thd.springboottest.standardcode.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+
+/**
+ * com.thd.springboottest.standardcode.controller.ValidatorController
+ *
+ * @author: wanglei62
+ * @DATE: 2020/5/6 9:16
+ **/
+@Controller
+@RequestMapping("/validator")
+public class ValidatorController {
+    @RequestMapping("/test")
+    // url :http://127.0.0.1:8899/thd/validator/test
+    // requestbody:
+    /**
+     * {
+     * 	"id":1,
+     * 	"account":"1237",
+     * 	"password":"abcdefghti",
+     * 	"email":"aaaab"
+     * }
+     */
+    public ResponseEntity test(@RequestBody @Valid com.thd.springbootidtest.standardcode.bean.User user, BindingResult bindingResult){
+        System.out.println("test");
+        String errorsInfo = "";
+        // 如果有参数校验失败，会将错误信息封装成对象组装在BindingResult里
+        for (ObjectError error : bindingResult.getAllErrors()) {
+            errorsInfo += error.getDefaultMessage() + ";";
+        }
+
+        return ResponseEntity.ok(errorsInfo);
+    }
+
+
+    // 使用ExceptionHandler 来处理验证错误的异常信息 切面类为MVCExceptionHandler,方法是handleMethodArgumentNotValidException
+    @RequestMapping("/test02")
+    // url :http://127.0.0.1:8899/thd/validator/test02
+    // requestbody:
+    /**
+     * {
+     * 	"id":1,
+     * 	"account":"1237",
+     * 	"password":"abcdefghti",
+     * 	"email":"aaaab"
+     * }
+     */
+    public ResponseEntity test02(@RequestBody @Valid com.thd.springbootidtest.standardcode.bean.User user){
+        System.out.println("test02");
+
+        return ResponseEntity.ok(user);
+    }
+
+}
--- com-thd-springboottest-validator/src/main/java/com/thd/springboottest/validator/advice/MVCExceptionHandler.java
+++ com-thd-springboottest-validator/src/main/java/com/thd/springboottest/validator/advice/MVCExceptionHandler.java
@@ -1,15 +1,13 @@
-package com.thd.springboottest.standardcode.advice;
+package com.thd.springboottest.validator.advice;
 
-import com.thd.springboottest.standardcode.bean.Result;
-import com.thd.springboottest.standardcode.exceptiondefine.MyParentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -24,28 +22,9 @@ import java.util.List;
 public class MVCExceptionHandler {
     private final static Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
 
-    @ExceptionHandler(value = Exception.class)
-    @ResponseBody
-    public Result handle(Exception e) {
-        logger.info("进入error");
-
-        // 是否属于自定义异常
-
-        //System.out.println(e instanceof MyParentException);
-        if (e instanceof MyParentException) {
-            MyParentException myException = (MyParentException) e;
-            logger.error("系统异常 {}", e);
-            return Result.error(myException.getCode(), myException.getMessage());
-        } else {
-            logger.error("系统异常 {}", e);
-            return Result.error(1000, "系统异常:" + e.getMessage());
-        }
-    }
-
-
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     @ResponseBody
-    public Result handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         logger.info("进入error");
         // 从异常对象中拿到ObjectError对象
         List<ObjectError> objectErrors = e.getBindingResult().getAllErrors();
@@ -57,7 +36,7 @@ public class MVCExceptionHandler {
         }
 
 
-        return Result.error(1000, "系统异常:" + errorsInfo);
+        return ResponseEntity.ok("系统异常:" + errorsInfo);
 
     }
 }
