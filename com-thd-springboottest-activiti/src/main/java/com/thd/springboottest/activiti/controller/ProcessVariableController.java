e5f54b5adde0a27c9eb888e20d431472927e3de8
wanglei62 <wanglei62@lenovo.com>
2020-07-20 18:26:25 +0800

    流程实例变量例子

--- com-thd-springboottest-activiti/src/main/java/com/thd/springboottest/activiti/controller/ProcessVariableController.java
+++ com-thd-springboottest-activiti/src/main/java/com/thd/springboottest/activiti/controller/ProcessVariableController.java
@@ -21,6 +21,13 @@ import java.util.stream.Collectors;
 
 /**
  * assignee、并行网关、流程变量的使用
+ *  流程变量结论:
+ *
+ *  local的流程变量按照执行ID(executionId),待办ID设置后  是从流程、执行ID、待办ID往下覆盖的
+ *      task的localVariable 只能根据taskId查看
+ *      execution的localVariable 只能executionId查看
+ *
+ *  全局的流程变量按照执行ID(executionId),待办ID(TaskId)设置后 ,可以根据流程，执行ID和TaskId查看
  *
  * @author: wanglei62
  * @DATE: 2019/10/12 15:49
@@ -50,28 +57,70 @@ http://127.0.0.1:8899/thd/pv/finishTask/18
 http://127.0.0.1:8899/thd/pv/queryTask/01
 [{"taskId":"27","taskName":"design","assignee":"lisi","owner":null,"processInstanceId":"14"},{"taskId":"30","taskName":"develop","assignee":"wangwu","owner":null,"processInstanceId":"14"}]
 
+
+--------- 开始测试流程变量
+
+-- 设置全局的task变量
+http://127.0.0.1:8899/thd/pv/saveTaskVar/31/taskAGloab/taskA_global
+http://127.0.0.1:8899/thd/pv/saveTaskVar/34/taskBGloab/taskB_global
+
+-- 设置局部的task变量
+http://127.0.0.1:8899/thd/pv/saveTaskLocalVar/31/taskALocal/taskA_local
+http://127.0.0.1:8899/thd/pv/saveTaskLocalVar/34/taskBLocal/taskB_local
+
+-- 设置全局的execution变量
+http://127.0.0.1:8899/thd/pv/saveProcessVar/15/executeAGloab/executeA_global
+http://127.0.0.1:8899/thd/pv/saveProcessVar/29/executeBGloab/executeB_global
+
+-- 设置局部的execution变量
+http://127.0.0.1:8899/thd/pv/saveProcessLocalVar/15/executeALocal/executeA_local
+http://127.0.0.1:8899/thd/pv/saveProcessLocalVar/29/executeBLocal/executeB_local
+
+-- 查看全局的流程变量
+http://127.0.0.1:8899/thd/pv/showProcessVar/14
+{"taskAGloab":"taskA_global","tester":"zhaoliu","executeBGloab":"executeB_global","developer":"wangwu","designer":"lisi","taskBGloab":"taskB_global","executeAGloab":"executeA_global","direction":"back"}
+
+-- 查看execution局部变量
+http://127.0.0.1:8899/thd/pv/showProcessVar/15
+{"taskAGloab":"taskA_global","executeALocal":"executeA_local","tester":"zhaoliu","executeBGloab":"executeB_global","developer":"wangwu","designer":"lisi","taskBGloab":"taskB_global","executeAGloab":"executeA_global","direction":"back"}
+
+-- 查看execution局部变量
+http://127.0.0.1:8899/thd/pv/showProcessVar/29
+{"executeBLocal":"executeB_local","taskAGloab":"taskA_global","tester":"zhaoliu","executeBGloab":"executeB_global","developer":"wangwu","designer":"lisi","taskBGloab":"taskB_global","executeAGloab":"executeA_global","direction":"back"}
+
+-- 查看task局部变量
+http://127.0.0.1:8899/thd/pv/showTaskVar/31
+{"taskALocal":"taskA_local"}
+
+http://127.0.0.1:8899/thd/pv/showTaskVar/34
+{"taskBLocal":"taskB_local"}
+
+
+--------- 测试流程变量结束
+
+
+
 -- 完成代办
-http://127.0.0.1:8899/thd/pv/finishTask/27
+http://127.0.0.1:8899/thd/pv/finishTask/31
 
 -- 查看待办
 http://127.0.0.1:8899/thd/pv/queryTask/01
 [{"taskId":"30","taskName":"develop","assignee":"wangwu","owner":null,"processInstanceId":"14"}]
 
 -- 完成代办
-http://127.0.0.1:8899/thd/pv/finishTask/30
+http://127.0.0.1:8899/thd/pv/finishTask/34
 
 -- 查看待办
 http://127.0.0.1:8899/thd/pv/queryTask/01
 [{"taskId":"35","taskName":"test","assignee":"zhaoliu","owner":null,"processInstanceId":"14"}]
 
 -- 完成代办
-http://127.0.0.1:8899/thd/pv/finishTask/35
+http://127.0.0.1:8899/thd/pv/finishTask/47
 
 -- 查询待办
 http://127.0.0.1:8899/thd/pv/queryTask/01
 [{"taskId":"41","taskName":"assign","assignee":"zhangsan","owner":null,"processInstanceId":"14"}]
  */
-
 @Controller
 @RequestMapping(value="/pv")
 public class ProcessVariableController {
@@ -152,6 +201,58 @@ public class ProcessVariableController {
 
 
     /**
+     * 测试不同的流程变量设置
+     * @return
+     */
+    @RequestMapping(value="/saveProcessVar/{processInstanceId}/{name}/{value}")
+    @ResponseBody
+    // url : http://127.0.0.1:8899/thd/pv/saveProcessVar/14
+    public String saveProcessVar(@PathVariable String processInstanceId,@PathVariable String name,@PathVariable String value){
+        this.runtimeService.setVariable(processInstanceId,name,value);
+        return "SUCCESS";
+    }
+
+    /**
+     * 测试不同的流程变量设置
+     * @return
+     */
+    @RequestMapping(value="/saveProcessLocalVar/{processInstanceId}/{name}/{value}")
+    @ResponseBody
+    // url : http://127.0.0.1:8899/thd/pv/saveProcessLocalVar/14
+    public String saveProcessLocalVar(@PathVariable String processInstanceId,@PathVariable String name,@PathVariable String value){
+        this.runtimeService.setVariableLocal(processInstanceId,name,value);
+        return "SUCCESS";
+    }
+
+
+
+
+
+    /**
+     * 测试不同的流程变量设置
+     * @return
+     */
+    @RequestMapping(value="/saveTaskVar/{taskId}/{name}/{value}")
+    @ResponseBody
+    // url : http://127.0.0.1:8899/thd/pv/saveTaskVar/14
+    public String saveTaskVar(@PathVariable String taskId,@PathVariable String name,@PathVariable String value){
+        this.taskService.setVariable(taskId,name,value);
+        return "SUCCESS";
+    }
+    /**
+     * 测试不同的流程变量设置
+     * @return
+     */
+    @RequestMapping(value="/saveTaskLocalVar/{taskId}/{name}/{value}")
+    @ResponseBody
+    // url : http://127.0.0.1:8899/thd/pv/saveTaskLocalVar/27
+    public String saveTaskLocalVar(@PathVariable String taskId,@PathVariable String name,@PathVariable String value){
+        this.taskService.setVariableLocal(taskId,name,value);
+        return "SUCCESS";
+    }
+
+
+    /**
      * 查询某人任务
      * @param user
      * @return
@@ -198,6 +299,20 @@ public class ProcessVariableController {
         Map m = this.runtimeService.getVariables(processInstanceId);
         return m;
     }
+
+    /**
+     * 查看待办的流程变量
+     * @param taskId 流程实例ID
+     * @return
+     */
+    // url : http://127.0.0.1:8899/thd/pv/showTaskVar/14
+    @RequestMapping(value="/showTaskVar/{taskId}")
+    @ResponseBody
+    public Map showTaskVar(@PathVariable String taskId){
+        Map m = this.taskService.getVariablesLocal(taskId);
+        return m;
+    }
+
 
 
 
